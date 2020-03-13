a1eeffe7770450f41cf0abb44ec5edc10fdcd2fe
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-13 15:58:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b 
+  end
+end
 
+def sort_array_desc(array)
+  array.sort.reverse do |a, b|
+    a <=> b 
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length 
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+  
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |kesha|
+    kesha[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select {|a| a.start_with?("a")}
+end
+
+#def sum_array(array)
+#  array.sum
+#end
+
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
+end
